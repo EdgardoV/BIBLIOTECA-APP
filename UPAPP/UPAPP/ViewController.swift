2a6228435ac7fd34ae5aefbbe62c5b1c5f8cb609
Edgardo Victorino Marin <edgardovictorinomarin@MacBook-Pro-de-Edgardo.local>
2018-05-26 19:37:01 -0500

    Se agrega conexion a firebase se prepara la pantalla de multas y se genere el workspace en el que se va a trabajar

--- UPAPP/UPAPP/ViewController.swift
+++ UPAPP/UPAPP/ViewController.swift
@@ -7,31 +7,38 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var consulta_multas: UITableView!
+    var handler:DatabaseHandle?
+    var ref: DatabaseReference?
+    var multas_lbl:[String] = []
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ref = Database.database().reference()
+        
+        handler = ref?.child("multa/150009_001").observe(.childAdded, with: {(snapshot) in
+            if let item = snapshot.value as? String
+            {
+                self.multas_lbl.append(item)
+                self.consulta_multas.reloadData()
+            }
+        })
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return multas.count
+        return multas_lbl.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let celda = multas_list.dequeueReusableCell(withIdentifier: "multa")
-        celda?.textLabel?.text = multas[0].capitalized
-        return celda!
-    }
-    
-
-    var multas = [String]()
-    var i = 0
-    @IBOutlet weak var multas_list: UITableView!
-    @IBOutlet weak var buscador: UISearchBar!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        //multas.append("Nombre del libro de la multa 1")
-        multas.append("Nombre del libro de la multa 2")
-        multas.append("Nombre del libro de la multa 3")
-        multas.append("Nombre del libro de la multa 4")
-        multas_list?.delegate = self
-        multas_list?.dataSource = self
+        let multa_celda = UITableViewCell(style: .default, reuseIdentifier: "multa_desc")
+        multa_celda.textLabel?.text = multas_lbl[indexPath.row]
+        return multa_celda
     }
     
     override func didReceiveMemoryWarning() {
