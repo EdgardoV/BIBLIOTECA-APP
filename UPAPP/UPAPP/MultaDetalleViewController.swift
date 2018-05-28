607b1d893a6e85394b077534f8e6f83ae755916c
Edgardo Victorino Marin <edgardovictorinomarin@MacBook-Pro-de-Edgardo.local>
2018-05-27 19:49:41 -0500

    Se termina pantalla de detalle de multas

--- UPAPP/UPAPP/MultaDetalleViewController.swift
+++ UPAPP/UPAPP/MultaDetalleViewController.swift
@@ -6,29 +6,110 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class MultaDetalleViewController: UIViewController {
-
+    @IBOutlet weak var fecha_lbl: UILabel!
+    @IBOutlet weak var nombre_lbl: UILabel!
+    @IBOutlet weak var costo_lbl: UILabel!
+    
+    var handler:DatabaseHandle?
+    var ref: DatabaseReference?
+    
+    var multas_desc = [
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ["","","","",""],
+        ]
+    
+    var indice = Int()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        ref = Database.database().reference()
+        
+        for index in 1...49{
+            
+            handler = ref?.child("multa").child("150009/\(index)").observe(DataEventType.value, with: {(snapshot) in
+                let value = snapshot.value as? NSDictionary
+                if value != nil{
+                    let descripcion = value?["descripcion"] as? String ?? ""
+                    let fecha = value?["fecha"] as? String ?? ""
+                    let costo = value?["costo"] as? Double ?? 0
+                    let estatus = value?["estatus"] as? String ?? ""
+                    let nombre_libro = value?["libro"] as? String ?? ""
+                    if estatus == "pendiente" {
+                        print("OKOK")
+                        self.multas_desc[index][0] = descripcion
+                        self.multas_desc[index][1] = String(costo)
+                        self.multas_desc[index][2] = estatus
+                        self.multas_desc[index][3] = nombre_libro
+                        self.multas_desc[index][4] = fecha
+                        print("index: \(index) indice: \(self.indice)")
+                        
+                        print("\(self.multas_desc[index][0]) \(self.multas_desc[index][1]) \(self.multas_desc[index][2]) \(self.multas_desc[index][3]) \(self.multas_desc[index][4])")
+                        if index == self.indice {
+                            self.fecha_lbl.text = self.multas_desc[self.indice][4]
+                            self.nombre_lbl.text = self.multas_desc[self.indice][3]
+                            self.costo_lbl.text = self.multas_desc[self.indice][1]
+                        }
+                    }
+                }
+                
+            })
+        }
+        
 
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
